c8be962c144083a1f36033f2daa1d363a7eb616a
Øystein Dale <oystedal@ifi.uio.no>
2016-10-30 03:20:45 +0100

    txn: extern C for when compiling with C++

--- src/txn.h
+++ src/txn.h
@@ -3,6 +3,10 @@
 
 #include "core.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 int txn_begin(edb *db);
 int txn_begin_master(edb *db);
 int txn_allocate_block(edb *db, u32 *new_block);
@@ -11,5 +15,9 @@ int txn_free_block(edb *db, u32 block);
 int txn_abort(edb *db);
 int txn_commit(edb *db);
 int txn_commit_master(edb *db);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif /* ifndef TXN_H */
