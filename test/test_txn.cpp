3abf7ea147e60121d4807de74c1fe013756d3e64
Øystein Dale <oystedal@ifi.uio.no>
2016-11-18 22:31:01 +0100

    test: added new txn tests

--- test/test_txn.cpp
+++ test/test_txn.cpp
@@ -2,6 +2,8 @@
 
 #include "../src/edb.h"
 #include "../src/txn.h"
+#include "../src/page.h"
+#include "../src/array.h"
 
 typedef struct txn_state_t {
   u32 nblocks;
@@ -19,4 +21,57 @@ TEST_CASE("txn") {
   REQUIRE(txn_begin(db) == 0);
   REQUIRE(db->txn != NULL);
   REQUIRE(db->txn->next == NULL);
+
+  REQUIRE(txn_begin(db) == 0);
+  REQUIRE(db->txn != NULL);
+  REQUIRE(db->txn->next != NULL);
+
+  REQUIRE(txn_commit(db) == 0);
+  REQUIRE(db->txn != NULL);
+  REQUIRE(db->txn->next == NULL);
+
+  REQUIRE(txn_commit(db) == 0);
+  REQUIRE(db->txn == NULL); // Fails
+  REQUIRE(txn_commit(db) != 0); // Segfaults
+
+  edb_close(db);
+}
+
+TEST_CASE("txn_trivial_commit") {
+  edb *db = new edb;
+
+  REQUIRE(edb_open(db, "test_txn_trivial_commit.db", 0, 1) == 0);
+  REQUIRE(db->txn == NULL);
+
+  REQUIRE(txn_begin(db) == 0);
+  REQUIRE(edb_resize(db, 2) == 0);
+  REQUIRE(db->nblocks == 2);
+
+  REQUIRE(txn_begin(db) == 0);
+  REQUIRE(edb_resize(db, 8) == 0);
+  REQUIRE(txn_commit(db) == 0);
+
+  REQUIRE(db->nblocks == 8);
+
+  edb_close(db);
+}
+
+TEST_CASE("txn_trivial_abort") {
+  edb *db = new edb;
+
+  REQUIRE(edb_open(db, "test_txn_trivial_abort.db", 0, 1) == 0);
+  REQUIRE(db->txn == NULL);
+
+  REQUIRE(txn_begin(db) == 0);
+
+  REQUIRE(edb_resize(db, 2) == 0);
+  REQUIRE(db->nblocks == 2);
+
+  REQUIRE(txn_begin(db) == 0);
+
+  REQUIRE(edb_resize(db, 8) == 0);
+  REQUIRE(txn_abort(db) == 0);
+  REQUIRE(db->nblocks == 2);
+
+  edb_close(db);
 }
